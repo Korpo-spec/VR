af07cc7e955358e838b0aec07f4e47c9ed2b2100
Korpo-spec <casper.sandstrom@elev.ga.ntig.se>
2020-02-17 15:00:22 +0100

    Initial Commit

---
+++ Assets/Scripts/SpawnCube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class SpawnCube : MonoBehaviour
+{
+    public ISteamVR_Action_Boolean gripAction;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool hey = gripAction.state;
+
+        print(hey);
+    }
+}
